3873a533ff26041358379745dcec268fbdcabe0c
S1ii <ilya.sitnikov29@gmail.com>
2025-04-29 16:30:50 +0500

    Удалены компоненты OrganizationDetail, OrganizationForm, OrganizationGrid и OrganizationTable. Обновлен подмодуль client до версии 1965cdfe. Обновлена база данных SQLite.

--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
 @echo off
-echo Starting Dariga application...
+echo Starting application...
 
 :: Start server with migration
-start "Dariga Server" cmd /k "cd server && npm start migrate"
+start "Server" cmd /k "cd server && npm start migrate"
 
 :: Start client
-start "Dariga Client" cmd /k "cd client && npm start"
+start "Client" cmd /k "cd client && npm start"
 
 echo Applications are starting... 
